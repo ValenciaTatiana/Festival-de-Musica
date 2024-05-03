7f83cad7d13a2621f861463af74891ea75614e8f
Tatiana Cuadros <tatianacuadros31@gmail.com>
2024-05-02 23:12:14 -0500

    Add sticky navigation on hover and select link on hover.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,22 @@
 // Evento para que una vez el HTML este cargado se llame a la función crearGaleria()
 document.addEventListener('DOMContentLoaded', function () {
+    navegacionFija();
     crearGaleria();
+    resaltarEnlaces();
 })
+
+function navegacionFija() {
+    const header = document.querySelector(".header");
+    const sobreFestival = document.querySelector(".sobre-festival");
+
+    window.addEventListener("scroll", function() {
+        if(sobreFestival.getBoundingClientRect().bottom < 1) {
+            header.classList.add("fixed");
+        } else {
+            header.classList.remove("fixed");
+        }
+    })
+}
 
 function crearGaleria() {
     const galeria = document.querySelector(".galeria-imagenes");
@@ -57,4 +72,28 @@ function cerrarModal() {
         const body = document.querySelector("body");
         body.classList.remove("overflow-hidden")
     }, 500)
+}
+
+function resaltarEnlaces() {
+    document.addEventListener('scroll', function() { 
+        const sections = document.querySelectorAll("section");
+        const navLinks = document.querySelectorAll(".navegacion-principal a");
+
+        let actual = '';
+        sections.forEach( section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if(window.scrollY >= (sectionTop - sectionHeight / 3)) {
+                actual = section.id;
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove("active");
+            if(link.getAttribute("href") == "#" + actual) {
+                link.classList.add("active");
+            }
+        });
+    })
 }
